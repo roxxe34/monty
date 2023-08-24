b5f327e35a243599fc36212245501833bca8d025
roxxe34 <hamzafarissi7@gmail.com>
2023-08-24 03:47:17 +0100

    delete

--- bytecodes/000.m
+++
@@ -1,7 +0,0 @@
-push 6
-push 7
-push 8
-push 1000
-pall
-rotl
-pall
--- nop.c
+++
@@ -1,7 +0,0 @@
-#include "monty.h"
-
-void opcode_nop(stack_t **stack, unsigned int line_number)
-{
-    (void)stack;
-    (void)line_number;
-}
